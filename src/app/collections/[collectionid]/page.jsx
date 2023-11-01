fbdbcc698578ee03183fc177d11fb0c4d8de94e4
Prince <princegujjar1.pp@gmail.com>
2023-11-02 00:30:54 +0530

    Create nft

--- src/app/collections/[collectionid]/page.jsx
+++ src/app/collections/[collectionid]/page.jsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useEffect, useState, useMemo } from 'react'
 import { useRouter } from 'next/navigation'
-import Link from 'next/link'
 import { client } from '../../../../lib/sanityClient'
 import { useContract ,useDirectListings,useNFTs} from '@thirdweb-dev/react'
 import Header from '../../components/navbar'
--- src/app/create/page.js
+++ src/app/create/page.js
@@ -4,16 +4,19 @@ import Header from '../components/navbar'
 import { useMintNFT, useContract, Web3Button } from "@thirdweb-dev/react";
 import { useAuthContract } from '../Context/ContractContext';
 import toast, { Toaster } from 'react-hot-toast';
+import {useRouter} from 'next/navigation';
 export default function page() {
   const [name, setName] = useState('');
   const [supply, setSupply] = useState('');
   const [uri,setUri]=useState('');
   const [description, setDescription] = useState('');
-  const contract = useAuthContract()?.contract;
+  const contract = useAuthContract()?.contractInstance;
   const currentuser = useAuthContract()?.currentUser;
+ 
   const {nftsContract} =useContract("0x472E3f14B7cE81e4ff732360F2BBBF7198eD93c7");
   const { mutateAsync: mintNft, isLoading, error } = useMintNFT(nftsContract);
-  console.log(error);
+  const router = useRouter();
+
   useEffect(() => {
     if(!contract){
       router.push('/');
@@ -35,11 +38,23 @@ const handlePay = async () => {
       showError();
     }
   }, [error]);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here
-  };
 }
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the form from submitting and causing a page reload
+    // Handle form submission here
+  
+    // You can include your mintNft logic here or make an API call, etc.
+    console.log(name, uri, description, supply);
+    mintNft({
+      metadata: {
+        name: name,
+        description: description,
+        image: uri, // Accepts any URL or File type
+      },
+      to: currentuser, // Use useAddress hook to get the current wallet address
+    });
+  };
   return (
     <div> 
        <Toaster position="top-center" reverseOrder={false} />
@@ -47,7 +62,7 @@ const handlePay = async () => {
     <div className="relative min-h-screen before:content-[''] before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s250')] before:bg-cover before:bg-center before:opacity-30 before:blur before:pointer-events-none">
    
     <div className="nft-form bg-black text-white p-8 flex flex-col items-center justify-center min-h-screen">
-      <form  className="space-y-4">
+      <form  onSubmit={handleSubmit} className="space-y-4">
         <label htmlFor="name" className="block">
           <span className="text-sm">Name your NFT*</span>
           <input
@@ -93,16 +108,7 @@ const handlePay = async () => {
         </label>
         <Web3Button
       contractAddress={"0x472E3f14B7cE81e4ff732360F2BBBF7198eD93c7"}
-      action={() =>
-        mintNft({
-          metadata: {
-            name: name,
-            description: description,
-            image: uri, // Accepts any URL or File type
-          },
-          to: currentuser, // Use useAddress hook to get current wallet address
-        })
-      }
+      action={handleSubmit}
     >
       Mint NFT
     </Web3Button>
